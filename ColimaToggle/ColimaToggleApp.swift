af11f93de288291ab2ce552becbf687125f4395c
kuafu <kuafu@u1s1.biz>
2025-05-26 21:17:13 +0400

    Initial Commit

---
+++ ColimaToggle/ColimaToggleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColimaToggleApp.swift
+//  ColimaToggle
+//
+//  Created by kuafu on 26.05.25.
+//
+
+import SwiftUI
+
+@main
+struct ColimaToggleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
